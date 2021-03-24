2835c8a7419e5e8ec51cafbee296d1ae1bf1cb5b
mohanjnv1 <mohanjnv1@gmail.com>
2021-03-24 18:46:27 +0530

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React,{useState,useEffect} from 'react';
 import  {Link} from 'react-router-dom';
 import {Link as Link1} from 'react-scroll';
-import {MdFingerprint}  from 'react-icons/md';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import {IconContext} from 'react-icons/lib';
 import {Button} from './Button/Button.js';
-import './Navbar.css';
+import classes from './Navbar.module.css';
 
 function Navbar() {
     const [click,setClick] =  useState(false);
@@ -29,46 +28,46 @@ function Navbar() {
     return (
         <>
         <IconContext.Provider value={{color:'#6B25D0'}}>
-            <div className="navbar">
-                <div className="navbar-container container">
-                    <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
+            <div className={classes.navbar}>
+                <div className={`${classes.navbar_container} ${classes.container}`}>
+                    <Link to='/' className={classes.navbar_logo} onClick={closeMobileMenu}>
                         {/* <MdFingerprint className='navbar-icon'/> */}
-                        <img src={process.env.PUBLIC_URL + '/Images/logo.svg'} className="navbar-icon" alt="logo" />
+                        <img src={process.env.PUBLIC_URL + '/Images/logo.svg'} className={classes.navbar_icon} alt="logo" />
                         CODINGCABLE
                     </Link>
-                    <div className="menu-icon" onClick={handleClick}>
+                    <div className={classes.menu_icon} onClick={handleClick}>
                     {click ? <FaTimes/> : <FaBars/>}
                     </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link1 to='home' onClick={closeMobileMenu} className="nav-links" smooth={true} duration={1000}>
+                    <ul className={click ? `${classes.nav_menu} ${classes.active}` : classes.nav_menu}>
+                        <li className={classes.nav_item}>
+                            <Link1 to='home' onClick={closeMobileMenu} className={classes.nav_links} smooth={true} duration={1000}>
                                 Home
                             </Link1>
                         </li>
-                        <li className='nav-item'>
-                            <Link1 to='services' onClick={closeMobileMenu} className="nav-links" smooth={true} duration={1000}>
+                        <li className={classes.nav_item}>
+                            <Link1 to='services' onClick={closeMobileMenu} className={classes.nav_links} smooth={true} duration={1000}>
                                 Courses
                             </Link1>
                         </li>
-                        <li className='nav-item'>
-                            <Link1 to='products' onClick={closeMobileMenu} className="nav-links" smooth={true} duration={1000}>
+                        <li className={classes.nav_item}>
+                            <Link1 to='products' onClick={closeMobileMenu} className={classes.nav_links} smooth={true} duration={1000}>
                                 Faculty
                             </Link1>
                         </li>
-                        <li className='nav-item'>
-                            <Link1 to='products' onClick={closeMobileMenu} className="nav-links" smooth={true} duration={1000}>
+                        <li className={classes.nav_item}>
+                            <Link1 to='products' onClick={closeMobileMenu} className={classes.nav_links} smooth={true} duration={1000}>
                                 About Us
                             </Link1>
                         </li>
-                        <li className='nav-btn' >
+                        <li className={classes.nav_btn} >
                             {button ? (
-                                <Link1 to='signup' className='btn-link' smooth={true} duration={1000}>
+                                <Link1 to='signup' className={classes.btn_link} smooth={true} duration={1000}>
                                     <Button buttonStyle='btn--rounded'>
                                         LogIn/SignUp
                                     </Button>
                                 </Link1>
                             ):(
-                                <Link1 to='signup' className='btn-link' onClick={closeMobileMenu} smooth={true} duration={1000}>
+                                <Link1 to='signup' className={classes.btn_link} onClick={closeMobileMenu} smooth={true} duration={1000}>
                                     <Button buttonStyle='btn--outline'
                                             buttonSize='btn--mobile'>
                                                 SIGN IN
